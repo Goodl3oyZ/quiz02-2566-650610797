f2b5eb82e74158d4df44e3e0893e9d86577af07e
Goodl3oyZ <panudetzx2@gmail.com>
2023-08-10 15:32:22 +0700

    not done

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,9 @@
 "use client";
 
+import { PostOwner } from "@/components/PostOwner";
+import { Comment } from "@/components/Comment";
+import { Reply } from "@/components/Reply";
+
 export default function HomePage() {
   return (
     <div
@@ -11,83 +15,39 @@ export default function HomePage() {
         style={{ maxWidth: "700px", backgroundColor: "#242526" }}
         className="mx-auto p-3 rounded rounded-3 shadow-sm"
       >
-        {/* Post Owner Example*/}
-        <div className="vstack gap-3">
-          <div className="d-flex align-items-center gap-3">
-            <img
-              src="/profileImages/handsome.jpg"
-              width="48"
-              height="48"
-              className="rounded-circle"
-              style={{ objectFit: "cover" }}
-            />
-            <span className="fw-semibold fs-5 text-white">
-              Chayanin Suatap 650610560
-            </span>
-          </div>
+        <PostOwner
+          postImagePath="/profileImages/myProfile.webp"
+          posterName="Phanudet Sueaphueak 650610797"
+          textPost="Quiz ง่ายจังเลยครับ ขอยาก ๆ กว่านี้ได้ไหม #261207"
+          likePost="100"
+        />
 
-          <span className="text-white">
-            Quiz ง่ายจังเลยครับ ขอยาก ๆ กว่านี้ได้ไหม #261207
-          </span>
+        <Comment
+          userImagePath="/profileImages/lisa.jpg"
+          username="Lisa"
+          replyText="จริงค่า"
+          likeNum="999"
+        />
 
-          <div className="d-flex align-items-center gap-1">
-            <img src="/like.svg" width={20}></img>
-            <span style={{ color: "#B0B3B8" }}>100 คน</span>
-          </div>
-          <hr className="m-0 border" />
-        </div>
+        <Reply
+          userImagePath="/profileImages/puppy.jpg"
+          username="หมาน้อย"
+          replyText="จริงค้าบบบบบบบบ"
+          likeNum="0"
+        />
 
-        {/* Comment Example */}
-        <div className="d-flex gap-2 my-2">
-          <img
-            src="/profileImages/lisa.jpg"
-            width="48"
-            height="48"
-            className="rounded-circle"
-            style={{ objectFit: "cover" }}
-          />
-          <div
-            className="rounded rounded-3 p-2"
-            style={{ backgroundColor: "#3A3B3C" }}
-          >
-            <span className="fw-semibold" style={{ color: "#E4E6EB" }}>
-              Lisa
-            </span>
-            <br />
-            <span style={{ color: "#E4E6EB" }}>จริงค่า</span>
-            <div className="d-flex align-items-center gap-1">
-              <img src="/like.svg" width={20}></img>
-              <span style={{ color: "#B0B3B8" }}>999 คน</span>
-            </div>
-          </div>
-        </div>
-
-        {/* Reply Example */}
-        <div className="d-flex gap-2 my-2 ps-5">
-          <img
-            src="/profileImages/puppy.jpg"
-            width="48"
-            height="48"
-            className="rounded-circle"
-            style={{ objectFit: "cover" }}
-          />
-          <div
-            className="rounded rounded-3 p-2"
-            style={{ backgroundColor: "#3A3B3C" }}
-          >
-            <span className="fw-semibold" style={{ color: "#E4E6EB" }}>
-              หมาน้อย
-            </span>
-            <br />
-            <span style={{ color: "#E4E6EB" }}>จริงค้าบบบบบบบบ</span>
-            <div className="d-flex align-items-center gap-1">
-              <img src="/like.svg" width={20}></img>
-              <span style={{ color: "#B0B3B8" }}>2 คน</span>
-            </div>
-          </div>
-        </div>
-
-        {/* map-loop render Comment component here */}
+        <Reply
+          userImagePath="/profileImages/popcat.png"
+          username="Cat Meme"
+          replyText="ลิซ่า"
+          likeNum="2"
+        />
+        <Comment
+          userImagePath="/profileImages/Charliebrown.jpg"
+          username="Charlie Brown"
+          replyText="บ้าไปแล้ว"
+          likeNum="0"
+        />
       </div>
     </div>
   );
